ca0e246ec8e9c05ed640aaf63ec3482bfe33ff76
Yasbernardi <92327526+Yasbernardi@users.noreply.github.com>
2022-01-03 19:18:20 -0300

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAAY7NmaxOBnPoH0hzGF54IPvRuQMx6hhs",
+  authDomain: "todo-list-427c4.firebaseapp.com",
+  projectId: "todo-list-427c4",
+  storageBucket: "todo-list-427c4.appspot.com",
+  messagingSenderId: "926065630714",
+  appId: "1:926065630714:web:ae9605a3b3cef7bd7c9843",
+  measurementId: "G-3BFVG0D2WP"
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
